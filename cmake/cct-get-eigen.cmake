1bcd421fea022bd68159a0f3777ea113dc9ca0ee
Jérémy Viau-Trudel <jeremy.vtrudel@cidco.ca>
2017-03-22 15:24:50 -0400

    provide a method to enable eigen

---
+++ cmake/cct-get-eigen.cmake
@@ -0,0 +1,11 @@
+# Option 1: user specified installed eigen library
+
+set(EIGEN3_CONFIG_DIR "" CACHE DIRECTORY "Directory containing Eigen3-config.cmake")
+
+if(NOT EIGEN3_CONFIG_DIR)
+    message(FATAL_ERROR "EIGEN3_CONFIG_DIR must be provided")
+endif()
+
+set(CMAKE_MODULE_PATH ${PROJECT_SOURCE_DIR}/cmake)
+
+find_package(Eigen3 REQUIRED PATHS ${EIGEN3_CONFIG_DIR} NO_DEFAULT_PATH)
