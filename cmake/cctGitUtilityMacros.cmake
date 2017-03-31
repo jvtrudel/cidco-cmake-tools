1802515a6dacfb5721985748a4af1552387eca6b
Jérémy Viau-Trudel <jeremy.vtrudel@cidco.ca>
2017-03-31 09:13:58 -0400

    fix git version variable + install directory management

--- cmake/cctGitUtilityMacros.cmake
+++ cmake/cctGitUtilityMacros.cmake
@@ -1,32 +1,36 @@
-if(NOT Git_FOUND)
-
-
-  # git version
-  find_package(Git)
-
-endif()
 
 
 macro(CCT_GET_SOURCE_VERSIONS)
 
-  # Get the latest abbreviated commit hash of the working branch
-  execute_process(
-    COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
-    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-    OUTPUT_VARIABLE _GIT_COMMIT_HASH
-    OUTPUT_STRIP_TRAILING_WHITESPACE
-  )
 
-  set(CCT_PROJECT_GIT_HASH _GIT_COMMIT_HASH CACHE INTERNAL "")
+    if(NOT Git_FOUND)
+
+      # get git package
+      find_package(Git)
+
+    endif()
+
+    # Get the current working branch
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      OUTPUT_VARIABLE GIT_BRANCH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+
+    # Get the latest abbreviated commit hash of the working branch
+    execute_process(
+      COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
+      WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+      OUTPUT_VARIABLE GIT_COMMIT_HASH
+      OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    message("GIT_COMMIT_HASH: ${GIT_COMMIT_HASH}")
+    message("GIT_BRANCH: ${GIT_BRANCH}")
+   set(CCT_PROJECT_GIT_HASH ${GIT_COMMIT_HASH} CACHE INTERNAL "")
+
 
 endmacro()
 
-#[[
-# Get the current working branch
-execute_process(
-  COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-  OUTPUT_VARIABLE _GIT_BRANCH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-]]#
--- cmake/cctInstallMacro.cmake
+++ cmake/cctInstallMacro.cmake
@@ -3,6 +3,12 @@
 
 macro(CCT_INSTALL)
 
+
+    if(CCT_INSTALL_ROOT)
+        set(CMAKE_INSTALL_PREFIX ${CCT_INSTALL_ROOT}/${CCT_PROJECT_NAME})
+        message("CMAKE_INSTALL_PREFIX: ${CMAKE_INSTALL_PREFIX}")
+    endif()
+
     # system installation
     include(InstallRequiredSystemLibraries)
     INSTALL(FILES ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS}
